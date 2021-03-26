3b98bdc5ae330e5db8e2e95ff504e2c3bd824e3f
Felipe Bezerra <felipe.f3rreira@gmail.com>
2021-03-26 15:13:10 +0000

    Modified file and Structure HTML for EJS

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,11 @@ const express = require('express');
 const routes = express.Router();
 
 
-routes.get('/', (request, response) => {
+const views = __dirname + '/views/';
 
-    console.log('Entrei no Index');
-
-    return response.sendFile(__dirname + '/views/index.html');
-
-});
+routes.get('/', (req, res) => res.render(views + 'index'));
+routes.get('/job', (req, res) => res.rende+ (views + 'job'));
+routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'));
+routes.get('/profile', (req, res) => res.render(views + 'profile'));
 
 module.exports = routes;
