c467304c9e0127913498c0c26d910967f03f4d15
Felipe Bezerra <felipe.f3rreira@gmail.com>
2021-03-26 16:53:01 +0000

    Finaly Structure Initial

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,20 @@ const routes = express.Router();
 
 const views = __dirname + '/views/';
 
-routes.get('/', (req, res) => res.render(views + 'index'));
-routes.get('/job', (req, res) => res.rende+ (views + 'job'));
+const profile = {
+
+    name: "Felipe Ferreira",
+    avatar: "https://github.com/FelipebFerreira.png",
+    
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+};
+
+routes.get('/', (req, res) => res.render(views + 'index', {profile}));
+routes.get('/job', (req, res) => res.render(views + 'job'));
 routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'));
-routes.get('/profile', (req, res) => res.render(views + 'profile'));
+routes.get('/profile', (req, res) => res.render(views + 'profile',{ profile }));
 
 module.exports = routes;
