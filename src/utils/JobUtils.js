ef43b191db4ac8525ac5d4d708f4a6cfdb5c5091
Felipe Bezerra <felipe.f3rreira@gmail.com>
2021-04-11 18:13:58 +0000

    modified archive JobUtils

--- src/utils/JobUtils.js
+++ src/utils/JobUtils.js
@@ -2,13 +2,14 @@ module.exports = {
     remainingDays(job) {
         /* toFixed aredonda o numero */
         const remainingDays = (job['total-hours'] / job['daily-hours']).toFixed();
-        const createdDate = new Date(job.createdAt);
+        const createdDate = new Date(job.created_at);
         const dueDay = createdDate.getDate() + Number(remainingDays);
+        console.log(createdDate);
         const dueDateInMs = createdDate.setDate(dueDay);
         const timeDiffInMs = dueDateInMs - Date.now();
         const dayInMs = 1000 * 60 * 60 * 24;
         const dayDiff = Math.floor(timeDiffInMs / dayInMs);
-
+        
         return dayDiff;
     },
     calculateBudget: (job , valueHour) => valueHour * job['total-hours'],
